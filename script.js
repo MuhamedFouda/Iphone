21541d8e181131388d20e1288f5126b615b74e9f
Muhamed Ahmed Faheem Fouda <135878522+MuhamedFouda@users.noreply.github.com>
2023-06-08 18:41:33 +0300

    iphone

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let op=document.querySelector('.imgg');
+let container=document.querySelector('.container');
+
+
+
+function phones(phone){
+op.src=phone;
+}
+
+function colors(color){
+    container.style.background=color;
+
+}
+
+
+
+
+/*
+function click(src){
+    if(src==="img/0.png"){
+        document.body.style.background='gold'
+    }if (src=="img/1.png") {
+        document.body.style.background='skyblue'
+
+    } if(src=="img/2.png") {
+        document.body.style.background='gray'
+
+    }if(src=="img/3.png"){
+        document.body.style.background='antiquewhite'
+
+    }if(src=="img/4.png"){
+        document.body.style.background='red'
+
+    }else{
+        document.body.style.background='black'
+
+    }
+
+
+}
+*/
